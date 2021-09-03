113d4b3a28626efb9049b35d4bb98ddabaddb41f
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2021-09-03 17:07:43 +1000

    Question: 26

---
+++ RemoveDuplicatesfromSortedArray.js
@@ -0,0 +1,14 @@
+/** Question: 26
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let pointer = 0;
+    while (pointer < nums.length - 1) {
+        if (nums[pointer] === nums[pointer + 1]) {
+            nums.splice(pointer+1, 1);
+        } else {
+            pointer++;
+        }
+    }
+};
