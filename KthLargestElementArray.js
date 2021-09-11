bb10fe8bbae58e2fd8ef9aa97fc7396897c755d2
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2021-09-11 22:07:10 +1000

    Question 215

--- KthLargestElementArray.js
+++ KthLargestElementArray.js
@@ -45,3 +45,61 @@ var findKthLargestQuickSelect = function(nums, k) {
     
     return nums[pivotIndex];
 };
+
+var isLeaf = function(nums, index) {
+    return (2 * index + 1) >= nums.length;     
+}
+
+var heapifyOneStep = function(nums, i) {
+    let temp = nums[i];
+    if (nums[2 * i + 1] < nums[i] || nums[2 * i + 2] < nums[i]) {
+        
+        if (nums[2 * i + 1] < nums[2 * i + 2] || (2*i+2)>nums.length-1) {
+            
+            nums[i] = nums[2 * i + 1];
+            nums[2 * i + 1] = temp;
+            return 2 * i + 1;
+            } else if (nums[2 * i + 1] >= nums[2 * i + 2]) {
+                nums[i] = nums[2 * i + 2];
+                nums[2 * i + 2] = temp;
+                return 2 * i + 2;
+            }
+        }
+    return null; //No need to heapify
+}
+
+var heapify = function(nums, i) {
+    result = heapifyOneStep(nums, i);
+    while (result !== null) {
+        result = heapifyOneStep(nums, result);
+    }
+}
+
+/**
+  *Assume it is a min-heap
+*/
+var heapifyConstruct = function(nums) {
+    for (let i = nums.length - 1; i > -1; i--) {
+        if (!isLeaf(nums,i)) {
+            heapify(nums, i);
+        }
+    }
+}
+
+var heapifyReplace = function(nums, value) {
+    nums[0] = value;
+    if (!isLeaf(nums, 0)){
+        heapify(nums, 0);
+    }
+}
+
+var findKthLargestHeap = function(nums, k) {
+    let tempHeap = nums.slice(0, k);
+    heapifyConstruct(tempHeap);
+    for (let i = k; i < nums.length; i++) {
+        if (nums[i] > tempHeap[0]) {
+            heapifyReplace(tempHeap, nums[i]);
+        }
+    }
+    return tempHeap[0];
+}
