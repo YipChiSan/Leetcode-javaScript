6490a2406c1ba69afc5d3aefc552cf9e8e5813c4
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2021-09-15 11:57:34 +1000

    Question 146

--- LRUcache.js
+++ LRUcache.js
@@ -1,96 +1,91 @@
-function ListNode(val, next, prev) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
-    this.prev = (prev===undefined ? null : prev)
+class ListNode {
+    constructor(key, val, next, prev){
+        this.key = (val===undefined ? 0 : key)
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+        this.prev = (prev===undefined ? null : prev)
+    }
  }
 
-/**
- * @param {number} capacity
- */
-var LRUCache = function(capacity) {
-    this.capacity = capacity;
-    this.dict = new Map();
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-};
-
-/** 
- * @param {number} key
- * @return {number}
- */
-LRUCache.prototype.get = function(key) {
-    if (this.dict.get(key) === undefined) {
-        return -1;
-    } else {
-        let node = this.dict.get(key);
-        if (node !== this.head) {
-            if (node !== this.tail) {
-                node.next.prev = node.prev;
-            } else {
-                this.tail = node.prev;
-            }
-            node.prev.next = node.next;
-            node.prev = null;
+class DoublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    
+    addNode(node) {
+        if (this.size > 0){
             node.next = this.head;
             this.head.prev = node;
-            this.head = node;
-        }
-        return node.val[1];
-    }
-};
-
-/** 
- * @param {number} key 
- * @param {number} value
- * @return {void}
- */
-LRUCache.prototype.put = function(key, value) {
-    let node;
-    if (this.dict.get(key) === undefined) {
-        if(this.length === this.capacity) {
-            let tailedKey = this.tail.val[0];
-            if (this.length !== 1) {
-                this.tail.prev.next = null;
-            } else {
-                this.head = null;
-            }
-            this.tail = this.tail.prev;
-            this.dict.delete(tailedKey);
-            this.length--;
-        }
-    
-        if (this.head !== null) {
-            node = new ListNode([key, value], this.head, null);
-            this.head.prev = node;
         } else {
-            node = new ListNode([key, value], null, null);
             this.tail = node;
         }
         this.head = node;
-        this.dict.set(key, node);
-        this.length++;
-        } else {
-            node = this.dict.get(key);
-            if (node !== this.head) {
-                if (node !== this.tail) {
-                    node.next.prev = node.prev;
-                } else {
-                    this.tail = node.prev;
-                }
-                node.prev.next = node.next;
-                node.prev = null;
-                node.next = this.head;
-                this.head.prev = node;
-                this.head = node;
-            } 
-            this.head.val = [key, value];
+        this.size++;
+    }
+    
+    removeTail() {
+        let key = this.tail.key;
+        if (this.size == 1) {
+            this.head = null;
         }
-};
+        this.tail = this.tail.prev;
+        this.size--;
+        return key;
+    }
+    
+    removeNode(node) {
+        if (node == this.head) {
+            this.head = this.head.next;
+        } else if (node == this.tail) {
+            this.tail = this.tail.prev;
+            this.tail.next = null;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+    
+     
+        
+        this.size--;
+    }
+}
 
-/** 
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */
+class LRUCache{
+    constructor(capacity){
+        this.dict = new Map();
+        this.linkedList = new DoublyLinkedList();
+        this.capacity = capacity;
+    }
+    
+    put(key, value) {
+        let node = new ListNode(key, value);
+        
+        if (this.dict.has(key)) {
+            
+            this.linkedList.removeNode(this.dict.get(key));
+            this.dict.delete(key);
+        } else{
+            if (this.capacity == this.linkedList.size) {
+              
+                this.dict.delete(this.linkedList.removeTail());
+            }
+        }
+        
+        this.linkedList.addNode(node);
+        this.dict.set(key, node);
+    }
+    
+    get(key) {
+        let node = this.dict.get(key);
+        if (node == undefined) {
+            return -1;
+        } else {
+            let value = node.val;
+            this.put(node.key, value);
+            return value;
+            
+        }
+    }
+}
