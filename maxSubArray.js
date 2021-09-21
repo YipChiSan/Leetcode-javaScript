982f41e0163279d2421795459e12eb9f412c847a
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2021-09-21 12:56:24 +1000

    Question 53

--- maxSubArray.js
+++ maxSubArray.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+var maxSubArrayLinear = function(nums) {
     let maxSum = Number.NEGATIVE_INFINITY;
     let sumToHere = 0;
     for (let i = 0; i < nums.length; i++) {
@@ -19,3 +19,42 @@ var maxSubArray = function(nums) {
     }
     return maxSum;
 };
+
+var maxCrossingSum = function(nums, l, m, r) {
+    let sum = 0;
+    let leftSum = Number.NEGATIVE_INFINITY;
+    let rightSum = Number.NEGATIVE_INFINITY;
+    for (let i = m; i > -1; i--) {
+        sum += nums[i];
+        
+        if(sum > leftSum) {
+            leftSum = sum;
+        }
+    }
+    sum = 0;
+    for (let i = m + 1; i < nums.length; i++) {
+        sum += nums[i];
+        
+        if(sum > rightSum) {
+            rightSum = sum;
+        }
+    }
+    
+    return Math.max(leftSum + rightSum, leftSum, rightSum);
+}
+
+var maxSubArrayRecur = function(nums, l, r) {
+    if(l == r) {
+        return nums[l];
+    }
+    
+    let m = Math.floor((l + r) / 2);
+    return Math.max(maxSubArrayRecur(nums, l, m),
+                    maxSubArrayRecur(nums, m + 1, r),
+                    maxCrossingSum(nums, l, m, r));
+    
+}
+
+var maxSubArray = function(nums) {
+    return maxSubArrayRecur(nums, 0, nums.length - 1);
+}
