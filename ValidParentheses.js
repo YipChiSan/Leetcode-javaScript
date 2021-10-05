45ca145b5ab6be663ccee0fe8623525fcaaf9c83
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2021-10-05 17:13:48 +1100

    Question 20

---
+++ ValidParentheses.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    let currentBracket;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == "(" ||
+            s[i] == "{" || 
+            s[i] == "[") {
+            stack.push(s[i]);
+        } else {
+            if (stack.length == 0) {
+                return false;
+            } else {
+                currentBracket = stack.pop();
+                if (s[i] == ")") {
+                    if (currentBracket != "(") {
+                        return false;
+                    }
+                }
+                
+                if (s[i] == "}") {
+                    if (currentBracket != "{") {
+                        return false;
+                    }
+                }
+                
+                if (s[i] == "]") {
+                    if (currentBracket != "[") {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    
+    return stack.length == 0;
+};
