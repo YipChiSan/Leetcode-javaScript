b351bd636dda84bde47dfa2fd474ca71caebf401
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2022-01-11 12:09:19 +1100

    Question 958

--- isCompleteTree.js
+++ isCompleteTree.js
@@ -36,3 +36,38 @@ var getNumberNodes = function(root) {
         return (1 + getNumberNodes(root.left) + getNumberNodes(root.right));
     }
 }
+
+//With queue
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isCompleteTree = function(root) {
+    let end = false;
+    let queue = [root];
+    
+    while (queue.length != 0) {
+        let output = queue.shift();
+        if (output == null) {
+            end = true;
+        } else {
+            if (end) {
+                return false;
+            } else {
+                queue.push(output.left);
+                queue.push(output.right);
+            }
+        }
+        
+        
+    }
+    return true;
+};
