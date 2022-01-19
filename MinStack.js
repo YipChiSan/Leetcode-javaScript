0ef74ded39358e301192a9002c59561387d404b3
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2022-01-19 11:45:50 +1100

    Question 155

--- MinStack.js
+++ MinStack.js
@@ -50,3 +50,67 @@ MinStack.prototype.getMin = function() {
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
+
+
+var MinStack = function() {
+    this.min = 0;
+    this.stack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    if (this.stack.length == 0) {
+        this.min = val;
+        this.stack.push(val);
+    } else {
+        if (val < this.min) {
+            this.stack.push(2 * val - this.min);
+            this.min = val;
+        } else {
+            this.stack.push(val);
+        }
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    if (this.stack.length == 0) {
+        return null;
+    }
+    let result = this.stack.pop();
+    if (result < this.min) {
+        let temp = this.min;
+        this.min = 2 * this.min - result;
+        result = temp;
+    }
+    return result;
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+   
+    return (this.stack[this.stack.length - 1] < this.min) ? this.min : this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.min;
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
